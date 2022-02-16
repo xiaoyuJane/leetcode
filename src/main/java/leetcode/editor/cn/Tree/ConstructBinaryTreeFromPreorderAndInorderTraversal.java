086850b4f6c78311af25703deb9977c26f7dda50
xiaoyu_zhan <errorl@126.com>
2022-02-16 10:00:16 +0800

    TreeNode

---
+++ src/main/java/leetcode/editor/cn/Tree/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,106 @@
+package leetcode.editor.cn.Tree;
+
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder 和 inorder 均 无重复 元素 
+// inorder 均出现在 preorder 
+// preorder 保证 为二叉树的前序遍历序列 
+// inorder 保证 为二叉树的中序遍历序列 
+// 
+// Related Topics 树 数组 哈希表 分治 二叉树 
+// 👍 1419 👎 0
+
+import java.util.Arrays;
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        //递归终止条件
+        if (preorder.length ==0 ||inorder.length ==0) return null;
+
+        //将前序队列的第一个元素做为root
+        TreeNode root = new TreeNode(preorder[0]);
+
+        for (int i=0;i<inorder.length;i++){
+            if (preorder[0] == inorder[i]){
+                int[] pre_left = Arrays.copyOfRange(preorder,1,i+1);
+                int[] pre_right = Arrays.copyOfRange(preorder,i+1,preorder.length);
+                int[] in_left = Arrays.copyOfRange(inorder,0,i);
+                int[] in_right = Arrays.copyOfRange(inorder,i+1,inorder.length);
+
+                //递归获取root的左右节点
+                root.left = buildTree(pre_left,in_left);
+                root.right = buildTree(pre_right,in_right);
+                break; //跳出，不再执行整个for循环了
+            }
+        }
+        return  root;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
