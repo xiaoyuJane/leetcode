767eecfbfacfcf75121ab2db5be46a7854fa6105
xiaoyu_zhan <errorl@126.com>
2022-02-17 15:00:20 +0800

    leetcode解题模板

---
+++ src/main/java/leetcode/editor/cn/Template.java
@@ -0,0 +1,13 @@
+package leetcode.editor.cn;
+
+public class Template {
+    public static void main(String[] args) {
+        Solution solution = new Template().new Solution();
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+}
