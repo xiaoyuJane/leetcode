783f895da19de3ee074f55e79080992eeed5a053
xiaoyu_zhan <errorl@126.com>
2022-02-18 09:47:01 +0800

    自定义最小栈数据结构，增加注释解释

--- src/main/java/leetcode/editor/cn/Stack/MinStack.java
+++ src/main/java/leetcode/editor/cn/Stack/MinStack.java
@@ -2,6 +2,10 @@ package leetcode.editor.cn.Stack;
 
 import java.util.Stack;
 
+/**
+ * 想要常量化时间复杂度求取栈中最小值，那就设置一个数据结构实时存储min值，这样就能从peek处拿到节点中的min值了
+ */
+
 //leetcode submit region begin(Prohibit modification and deletion)
     class MinStack {
         private Stack<StackNode> stack = new Stack<>();
@@ -36,7 +40,10 @@ import java.util.Stack;
 
     }
 
-    class StackNode{
+/**
+ * 针对题目要求，自定义了栈的数据结构，用min来存储栈中的最小值，这样就能实现常数时间获取最小值了
+ */
+class StackNode{
         public int val;
         public int min;
 
