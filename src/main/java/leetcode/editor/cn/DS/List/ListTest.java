9a8bf1e024942816546083e68eadbb4e69e97d6f
xiaoyu_zhan <errorl@126.com>
2022-04-07 11:26:18 +0800

    list相关使用

---
+++ src/main/java/leetcode/editor/cn/DS/List/ListTest.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn.DS.List;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list); //[1, 2, 3]
+
+        //拷贝一个list
+        List<Integer> list1 = new LinkedList<>(list);
+        System.out.println(list1); //[1, 2, 3]
+        
+
+        list.remove(list.size()-1);
+        System.out.println(list);  //[1, 2]
+
+
+    }
+}
