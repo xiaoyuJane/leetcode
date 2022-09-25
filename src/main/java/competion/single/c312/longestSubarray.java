dff8d4030794da8d817aaaaa76da7da56b94c450
xiaoyu_zhan <errorl@126.com>
2022-09-25 11:25:51 +0800

    周赛306

---
+++ src/main/java/competion/single/c312/longestSubarray.java
@@ -0,0 +1,16 @@
+package competion.single.c312;
+
+public class longestSubarray {
+    public static void main(String[] args) {
+        Solution solution = new longestSubarray().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int longestSubarray(int[] nums) {
+            return  0;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+}
---
+++ src/main/java/competion/single/c312/numberOfGoodPaths.java
@@ -0,0 +1,18 @@
+package competion.single.c312;
+
+public class numberOfGoodPaths {
+    public static void main(String[] args) {
+        Solution solution = new numberOfGoodPaths().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numberOfGoodPaths(int[] vals, int[][] edges) {
+            int res = 0;
+            return res;
+
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+}
