683d0b254586a98964bf3297e7785ee0f438f6f3
xiaoyu_zhan <errorl@126.com>
2022-09-30 18:39:54 +0800

    1。hashmap的遍历， 2.寻找第一个插入元素 3. map value不存在则返回默认值

--- src/main/java/TestAndTool/Temp.java
+++ src/main/java/TestAndTool/Temp.java
@@ -272,5 +272,89 @@ public class Temp {
     }
 
 
+    @Test
+    //maps.getOrDefault(c,0) 不存在则返回0，存在则返回get到的值
+    public void testMap(){
+        HashMap<Character, Integer> maps = new HashMap<>();
+        String s = "hello";
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            if (maps.containsKey(c)) maps.put(c,maps.get(c)+1);
+//            else maps.put(c,1);
+//        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            maps.put(c,maps.getOrDefault(c,0)+1);
+        }
+
+        maps.forEach((key,value) ->{
+            System.out.println("key is "+ key);
+            System.out.println("value is "+ value);
+        });
+
+
+    }
+
+
+    @Test
+    //map的遍历
+    public void reverseMap(){
+        //一般来说,最好初始化一下, 小于12的就不要初始化了
+        // 默认的就是16,因为加载因子是0.75,也就是到16*0.75=12的时候会扩容
+        Map<String, String> map = new HashMap<>(3);
+
+        map.put("welcome","to");
+        map.put("java","study");
+        map.put("wechat","best396975802");
+
+        //遍历方法1: 先遍历key , 再取出value
+        System.out.println("遍历方法1: 先遍历key , 再取出value");
+        for (String key : map.keySet()) {
+            System.out.println("key is "+key);
+            System.out.println("value is "+ map.get(key));
+        }
+
+        //遍历方法2: 直接遍历value
+        System.out.println("遍历方法2: 直接遍历value");
+        for (String value : map.values()) {
+            System.out.println("value is "+value);
+        }
+
+        //遍历方法3: 通过遍历entry来取Key和value,推荐的方法!!!
+        System.out.println("遍历方法3: 通过遍历entry来取Key和value,推荐的方法!!!");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println("key is "+entry.getKey());
+            System.out.println("value is "+ entry.getValue());
+        }
+
+        //遍历方法4: 通过forEach方法直接遍历key和value
+        System.out.println("遍历方法4: 通过forEach方法直接遍历");
+        map.forEach((key,value)->{
+            System.out.println("key is "+ key);
+            System.out.println("value is "+ value);
+        });
+    }
+
+
+    @Test
+    //LinkedHashMap 可以按照插入的顺序存储
+    public void testHashMap(){
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        map.put("welcome","to");
+        map.put("java","study");
+        map.put("wechat","best396975802");
+
+        Map.Entry entry = map.entrySet().iterator().next();
+        String key = (String)entry.getKey();
+        String value = (String)entry.getValue();
+        System.out.println("key is "+ key); //key is welcome
+        System.out.println("value is "+ value); //value is to
+
+    }
+
+
+
+
 
 }
