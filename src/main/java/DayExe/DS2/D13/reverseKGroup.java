73d314f3af0d87f2a3968bbf7d4faac5be8e1d3b
xiaoyu_zhan <errorl@126.com>
2022-10-21 23:27:49 +0800

    D13

---
+++ src/main/java/DayExe/DS2/D13/reverseKGroup.java
@@ -0,0 +1,56 @@
+package DayExe.DS2.D13;
+
+import TestAndTool.tool.ListNode;
+
+public class reverseKGroup {
+    public static void main(String[] args) {
+        Solution solution = new reverseKGroup().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode hair = new ListNode(0);
+            hair.next = head;
+            ListNode pre = hair;
+
+            while (head != null) {
+                ListNode tail = pre;
+                // 查看剩余部分长度是否大于等于 k
+                for (int i = 0; i < k; ++i) {
+                    tail = tail.next;
+                    if (tail == null) {
+                        return hair.next;
+                    }
+                }
+                ListNode nex = tail.next;
+                ListNode[] reverse = myReverse(head, tail);
+                head = reverse[0];
+                tail = reverse[1];
+                // 把子链表重新接回原链表
+                pre.next = head;
+                tail.next = nex;
+                pre = tail;
+                head = tail.next;
+            }
+
+            return hair.next;
+        }
+
+        public ListNode[] myReverse(ListNode head, ListNode tail) {
+            ListNode prev = tail.next;
+            ListNode p = head;
+            while (prev != tail) {
+                ListNode nex = p.next;
+                p.next = prev;
+                prev = p;
+                p = nex;
+            }
+            return new ListNode[]{tail, head};
+        }
+
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+}
